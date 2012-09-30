5c15f15dc314ade537cb272460e9c0d862cda206
Victor Zhu <victorszhu@Victor-Zhus-MacBook-Pro.local>
2012-09-30 13:23:03 -0700

    Added validations

--- localee/app/models/location.rb
+++ localee/app/models/location.rb
@@ -4,9 +4,14 @@ class Location < ActiveRecord::Base
   ###Validations
   validates :name, :presence => true
 
-  #TODO(kchan): include validation for bounds of GPS coordinates
-  validates :latitude, :presence => true
-  validates :longitude, :presence => true
+  #ADDED: included validation for bounds of GPS coordinates, can't test since VM isn't working...
+  validates :latitude, numericality: {
+   	    greater_than_or_equal_to: -89.999, less_than_or_equal_to: 89.999
+  }, :presence => true
+
+  validates :longitude, numericality: {
+  	    greater_than_or_equal_to: -179.999, less_than_or_equal_to: 179.999
+  }, :presence => true
 
   ### Associations
   has_many :posts
